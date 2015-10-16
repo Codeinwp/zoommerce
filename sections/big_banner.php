52916d6d9575c994516fd225c2d6158947c6b4b9
Hapiuc Robert <robert@9pixels.co>
2015-10-16 16:05:59 +0300

    big banner php file clean after fields remove

--- sections/big_banner.php
+++ sections/big_banner.php
@@ -9,129 +9,23 @@
 /**************  Background settings *************/
 /*************************************************/
 
-$zerif_background_settings = get_theme_mod('zerif_background_settings');
-
-/* Default case when no setting is checked or Slider is selected */
-if( empty($zerif_background_settings) || ($zerif_background_settings == 'zerif-background-slider') ):
-
-	/* Background slider */
-	$zerif_slides_array = array();
-
-	for ($i=1; $i<=3; $i++){
-		$zerif_bgslider = get_theme_mod('zerif_bgslider_'.$i);
-		array_push($zerif_slides_array, $zerif_bgslider);
-	}
-
-	$count_slides = 0;
-
-	if( !empty($zerif_slides_array) && is_home() ):		
-
-		echo '<div class="fadein-slider">';
-
-		foreach( $zerif_slides_array as $key => $zerif_slide ):
-
-			if ( !empty($zerif_slide) ):
-
-				$keyx = $key+1;
-				$zerif_vpos = get_theme_mod('zerif_vposition_bgslider_'.$keyx,'top');
-				$zerif_hpos = get_theme_mod('zerif_hposition_bgslider_'.$keyx,'left');
-				$zerif_bgsize = get_theme_mod('zerif_bgsize_bgslider_'.$keyx,'cover');
-				if ($zerif_bgsize=='width'):
-					$zerif_bgsize = '100% auto';
-				elseif ($zerif_bgsize=='height'):
-					$zerif_bgsize = 'auto 100%';
-				endif;
-
-				$zerif_slide_style ='background-repeat:no-repeat;background-position:'.$zerif_hpos.' '.$zerif_vpos.';background-size:'.$zerif_bgsize;
-
-				echo '<div class="slide-item" style="background-image:url('.$zerif_slide.');'.$zerif_slide_style.'"></div>';
-
-			endif;
-
-		endforeach;
-
-		echo '</div>';		
-
-	endif;
-
-elseif( $zerif_background_settings == 'zerif-background-video' ):
-
-	/* Video background */
-	$zerif_background_video = get_theme_mod('zerif_background_video');
-	if( !empty($zerif_background_video) && is_home() ):
-
-		$zerif_background_video_thumbnail = get_theme_mod('zerif_background_video_thumbnail');
-
-		if( !wp_is_mobile() ) {
-
-			if( !empty($zerif_background_video_thumbnail) ):
-
-				echo '<video class="zerif_video_background" loop autoplay preload="auto" controls="true" poster="'.$zerif_background_video_thumbnail.'" muted>';
-
-			else:
-
-				echo '<video class="zerif_video_background" loop autoplay preload="auto" controls="true" muted>';
-
-			endif;
-
-			echo '<source src="'.$zerif_background_video.'" type="video/mp4" />';
-			echo '</video>';
-
-		} else {
-
-			//echo '<div class="zerif_full_site_wrap">';
-
-			echo '<div class="fadein-slider">';
-
-			if( !empty($zerif_background_video_thumbnail) ) {
-
-				echo '<div class="slide-item" style="background-image:url('.$zerif_background_video_thumbnail.')"></div>';
-
-			} else {
-
-				$page_bg_image_url = get_background_image();
-
-				if ( !empty( $page_bg_image_url ) ) {
-
-					$page_bg_image_url = get_background_image();
-
-					echo '<div class="slide-item" style="background-image:url('.$page_bg_image_url.')"></div>';
-
-				}
-			}
-
-			echo '</div>';
-
-			//echo '<div class="zerif_full_site">';
-
-		}
-		
-	endif;
-	
-elseif( $zerif_background_settings == 'zerif-background-image' ):
-
 	/* Background Image */
 	$zerif_background_image = get_theme_mod('zerif_background_image');
 
-		echo '<div class="fadein-slider">';
-				
-		$zerif_vpos = get_theme_mod('zerif_vposition_bgimage','top');
-		$zerif_hpos = get_theme_mod('zerif_hposition_bgimage','left');
-		$zerif_bgsize = get_theme_mod('zerif_bgsize_bgimage','cover');
-		if ($zerif_bgsize=='width'):
-			$zerif_bgsize = '100% auto';
-		elseif ($zerif_bgsize=='height'):
-			$zerif_bgsize = 'auto 100%';
-		endif;
+	echo '<div class="fadein-slider">';
+			
+	$zerif_vpos = get_theme_mod('zerif_vposition_bgimage','top');
+	$zerif_hpos = get_theme_mod('zerif_hposition_bgimage','left');
+	$zerif_bgsize = get_theme_mod('zerif_bgsize_bgimage','cover');
+	if ($zerif_bgsize=='width'):
+		$zerif_bgsize = '100% auto';
+	elseif ($zerif_bgsize=='height'):
+		$zerif_bgsize = 'auto 100%';
+	endif;
 
-		$zerif_slide_style ='background-repeat:no-repeat;background-position:'.$zerif_hpos.' '.$zerif_vpos.';background-size:'.$zerif_bgsize;
+	$zerif_slide_style = 'background-repeat:no-repeat;background-position:'.$zerif_hpos.' '.$zerif_vpos.';background-size:'.$zerif_bgsize;
 
-		echo '<div class="slide-item" style="background-image:url('.$zerif_background_image.');'.$zerif_slide_style.'"></div>';
-
-		echo '</div>';
-		
-endif;
-	
+	echo '<div class="slide-item" style="background-image:url('.$zerif_background_image.');'.$zerif_slide_style.'"></div></div>';
 
 $zerif_bigtitle_show = get_theme_mod('zerif_bigtitle_show');
 	
