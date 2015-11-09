832429935805e8dca331fdc4e24881f7f55c6be6
Hapiuc Robert <robert@9pixels.co>
2015-11-09 12:16:40 +0200

    !!! home testimonials wp integration, section done

--- sections/home_testimonials.php
+++ sections/home_testimonials.php
@@ -6,126 +6,19 @@
 		</div><!-- / .home_headline -->
 		
 		<div class="testimonials_wrap owl-carousel">
-			<div class="feedback-box">
+			<?php
+			if(is_active_sidebar( 'sidebar-testimonials' )):
 
-				<!-- MESSAGE OF THE CLIENT -->
+				dynamic_sidebar( 'sidebar-testimonials' );
 
-				<div class="message">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ipsa quo illum excepturi autem voluptatem, maiores tempora quasi temporibus architecto ratione delectus modi qui cum earum, omnis itaque nam iure!
-				</div>
-
-				<!-- CLIENT INFORMATION -->
-
-				<div class="client">
-
-					<div class="quote red-text">
-					
-						<i class="fa fa-quote-left"></i>
-
-					</div>
-
-					<div class="client-info">
-
-						<a class="client-name" target="_blank" href="">Client name</a>
-						<div class="client-company">Client company</div>
-
-					</div>
-
-					<div class="client-image hidden-xs">
-						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/iflendra/128.jpg" alt="" />
-					</div>
-				</div> <!-- / END CLIENT INFORMATION-->
-			</div> <!-- / END SINGLE FEEDBACK BOX-->
-			<div class="feedback-box">
-
-				<!-- MESSAGE OF THE CLIENT -->
-
-				<div class="message">
-					1Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ipsa quo illum excepturi autem voluptatem, maiores tempora quasi temporibus architecto ratione delectus modi qui cum earum, omnis itaque nam iure!
-				</div>
-
-				<!-- CLIENT INFORMATION -->
-
-				<div class="client">
-
-					<div class="quote red-text">
-					
-						<i class="fa fa-quote-left"></i>
-
-					</div>
-
-					<div class="client-info">
-
-						<a class="client-name" target="_blank" href="">1Client name</a>
-						<div class="client-company">1Client company</div>
-
-					</div>
-
-					<div class="client-image hidden-xs">
-						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/iflendra/128.jpg" alt="" />
-					</div>
-				</div> <!-- / END CLIENT INFORMATION-->
-			</div> <!-- / END SINGLE FEEDBACK BOX-->
-			<div class="feedback-box">
-
-				<!-- MESSAGE OF THE CLIENT -->
-
-				<div class="message">
-					2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ipsa quo illum excepturi autem voluptatem, maiores tempora quasi temporibus architecto ratione delectus modi qui cum earum, omnis itaque nam iure!
-				</div>
-
-				<!-- CLIENT INFORMATION -->
-
-				<div class="client">
-
-					<div class="quote red-text">
-					
-						<i class="fa fa-quote-left"></i>
-
-					</div>
-
-					<div class="client-info">
-
-						<a class="client-name" target="_blank" href="">2Client name</a>
-						<div class="client-company">2Client company</div>
-
-					</div>
-
-					<div class="client-image hidden-xs">
-						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/iflendra/128.jpg" alt="" />
-					</div>
-				</div> <!-- / END CLIENT INFORMATION-->
-			</div> <!-- / END SINGLE FEEDBACK BOX-->
-			<div class="feedback-box">
-
-				<!-- MESSAGE OF THE CLIENT -->
-
-				<div class="message">
-					3Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ipsa quo illum excepturi autem voluptatem, maiores tempora quasi temporibus architecto ratione delectus modi qui cum earum, omnis itaque nam iure!
-				</div>
-
-				<!-- CLIENT INFORMATION -->
-
-				<div class="client">
-
-					<div class="quote red-text">
-					
-						<i class="fa fa-quote-left"></i>
-
-					</div>
-
-					<div class="client-info">
-
-						<a class="client-name" target="_blank" href="">3Client name</a>
-						<div class="client-company">3Client company</div>
-
-					</div>
-
-					<div class="client-image hidden-xs">
-						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/iflendra/128.jpg" alt="" />
-					</div>
-				</div> <!-- / END CLIENT INFORMATION-->
-			</div> <!-- / END SINGLE FEEDBACK BOX-->
+			else:
+			
+				the_widget( 'zerif_testimonial_widget','title=John Dow&text=Add a testimonial widget in the "Widgets: Testimonials section" in Customizer' );
+				the_widget( 'zerif_testimonial_widget','title=John Dow&text=Add a testimonial widget in the "Widgets: Testimonials section" in Customizer' );
+				the_widget( 'zerif_testimonial_widget','title=John Dow&text=Add a testimonial widget in the "Widgets: Testimonials section" in Customizer' );
+			
+			endif;
+			?>
 		</div><!-- / .testimonials_wrap owl-carousel -->
 	</div><!-- / .container -->
 </section><!-- /#etstimonials  -->
