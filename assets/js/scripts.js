40755cb93ee0c8baf14f1b6e2afe0e803f69d194
Hapiuc Robert <robert@9pixels.co>
2015-11-15 00:38:15 +0200

    !!! home 768 responsive done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -94,7 +94,7 @@ jQuery(document).ready(function($) {
 	homeBlogOwl.owlCarousel({
 		items : 2, //10 items above 1000px browser width
 		itemsDesktop : [1000,2], //5 items between 1000px and 901px
-		itemsDesktopSmall : [900,2], // betweem 900px and 601px
+		itemsDesktopSmall : [960,1], // betweem 900px and 601px
 		itemsTablet: [600,1], //2 items between 600 and 0
 		itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
 	});
